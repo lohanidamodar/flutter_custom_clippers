4bec96bf491713877479e984fb4e6b71277339e6
cimplesid <joshisid178@gmail.com>
2019-08-16 12:48:55 +0545

    modified sidecut clipper

--- lib/src/side_cut_clipper.dart
+++ lib/src/side_cut_clipper.dart
@@ -1,27 +1,72 @@
 import 'package:flutter/material.dart';
 
 class SideCutClipper extends CustomClipper<Path> {
-    final double depth ;
-    final double borderadius ;
-  SideCutClipper({this.borderadius=10,this.depth=40});
+  //play with scals to get more clear versions
   @override
   Path getClip(Size size) {
+    double xFactor = 18, yFactor = 15;
     double height = size.height;
-    double width = size.width;
-    double cut = height - height / 3;
-    double inDepth = width - depth;
-
+    double startY = (height - height / 3) - yFactor;
+    double xVal = size.width;
+    double yVal = 0;
     final path = Path();
-    // path(width,height)
-    path.lineTo(width, 0);
-    path.lineTo(width, cut - borderadius);
-    path.cubicTo(width, cut - borderadius, width, cut, width - borderadius,
-        cut + borderadius);
-    path.cubicTo(width - borderadius, cut + borderadius, inDepth,
-        cut + depth , width-borderadius, cut + 2*depth-borderadius);
-    path.cubicTo(width - borderadius, cut + 2*depth - borderadius, width,
-        cut + 2*depth, width, cut + 2*depth + borderadius);
-    path.lineTo(width, height);
+
+    path.lineTo(xVal, yVal);
+
+    yVal = startY;
+    path.lineTo(xVal, yVal);
+
+    double scale = 1.4;
+    path.cubicTo(xVal, yVal, xVal, yVal + yFactor * scale,
+        xVal - xFactor * scale, yVal + yFactor * scale);
+    xVal = xVal - xFactor * scale;
+    yVal = yVal + yFactor * scale;
+
+    double scale1 = 1;
+    path.cubicTo(xVal, yVal, xVal - xFactor * scale1, yVal,
+        xVal - scale1 * xFactor, yVal + yFactor * scale1);
+    xVal = xVal - scale1 * xFactor;
+    yVal = yVal + scale1 * yFactor;
+    double scale2 = 1.2;
+    path.cubicTo(xVal, yVal, xVal, yVal + yFactor * scale2,
+        xVal + xFactor * scale2, yVal + yFactor * scale2);
+    xVal = xVal + xFactor * scale2;
+    yVal = yVal + yFactor * scale2;
+
+    scale = 1.6;
+
+    path.cubicTo(xVal, yVal, xVal + xFactor * scale, yVal,
+        xVal + xFactor * scale, yVal + yFactor * scale);
+    xVal = xVal + xFactor * scale;
+    yVal = yVal + yFactor * scale;
+
+//need to recreate a better later
+    //first point in curve
+    // double test = 2.2;
+    // path.cubicTo(xVal, yVal, xVal, yVal + yFactor * test, xVal - xFactor * test,
+    //     yVal + yFactor * test);
+    // xVal = xVal - xFactor * test;
+    // yVal = yVal + yFactor * test;
+
+    // test = 1;
+    // path.cubicTo(xVal, yVal, xVal - xFactor * test, yVal, xVal - test * xFactor,
+    //     yVal + yFactor * test);
+    // xVal = xVal - test * xFactor;
+    // yVal = yVal + test * yFactor;
+
+    // path.cubicTo(xVal, yVal, xVal, yVal + yFactor * test, xVal + xFactor * test,
+    //     yVal + yFactor * test);
+    // xVal = xVal + xFactor * test;
+    // yVal = yVal + yFactor * test;
+
+    // test = 2.2;
+
+    // path.cubicTo(xVal, yVal, xVal + xFactor * test, yVal, xVal + xFactor * test,
+    //     yVal + yFactor * test);
+    // xVal = xVal + xFactor * test;
+    // yVal = yVal + yFactor * test;
+
+    path.lineTo(xVal, height);
     path.lineTo(0, height);
     path.close();
     return path;
